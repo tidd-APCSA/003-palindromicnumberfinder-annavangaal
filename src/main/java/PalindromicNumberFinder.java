21e5486cb83834d0168e15f457a47aa775d5706f
annavangaal <annavangaal@yahoo.com>
2020-11-07 21:46:54 +0000

    Made all the methods and created an odometer object that outputs next palindrome!

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -1,6 +1,7 @@
 public class PalindromicNumberFinder {
     private int num;
 
+
     // constructor
     public PalindromicNumberFinder(int num){
         this.num = num;
@@ -13,26 +14,40 @@ public class PalindromicNumberFinder {
 
     // this method should find the next palindromic number
     public int searchForPalindromicNum(int num){
-
+      int searchNum = num;
+      for(int i = 1; i > 0; i++){
+        searchNum += i;
+        if(testPalindromicNum(searchNum) == true){
+         break;
+        }
+      }
+      return searchNum;
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
-
-
+      Integer reverseInt = Integer.valueOf(reverseNum(num));
+      if(reverseInt.equals(num)){
+        return true;
+      }else{
+        return false;
+      }
     }
 
 
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
     public String reverseNum(int num){
-
-
-
+      String number = Integer.toString(num);
+      String reverse =  "";
+      for(int i = number.length()-1; i >= 0; i--){
+        reverse += number.charAt(i);
+      }
+      return reverse;
     }
 
 
-    @Override
-    public String toString(){
-        return "You'd have to drive " + searchForPalindromicNum(this.num) + " miles";
-    }
+    // @Override
+    // public String toString(){
+    //     return "You'd have to drive " + searchForPalindromicNum(this.num) + " miles";
+    // }
 }
